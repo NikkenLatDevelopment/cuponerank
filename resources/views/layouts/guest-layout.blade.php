f9a252236f00bb712c201bf57aa803e66286858d
gzamudioc <gzamudioc@nikkenlatam.com>
2024-05-13 13:16:15 -0600

    agrega estilos de Panel

--- resources/views/layouts/guest-layout.blade.php
+++ resources/views/layouts/guest-layout.blade.php
@@ -11,17 +11,18 @@
         
 
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-        <link href="{{ asset('css/apps.css') }}" rel="stylesheet" type="text/css">        
+        <link href="{{ asset('css/apps.css') }}" rel="stylesheet" type="text/css">
+        <link rel="shortcut icon" href="{{asset('images/favicon.ico')}}">        
        
         
         
     </head>
     <body>
 
-    {{ $slot }}
+        {{ $slot }}
 
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
         
     </body>
 </html>
--- resources/views/panel/index.blade.php
+++ resources/views/panel/index.blade.php
@@ -1,21 +1,74 @@
 <x-guest-Layout>
 
-<nav class="navbar bg-body-tertiary nvar-panel">
-    <div class="container">
-        <a class="navbar-brand" href="#">
-            <img src="{{asset('images/logo-nikken.png')}}" alt="NIKKEN">
-        </a>
-        <a class="navbar-brand">Navbar</a>
-        <form class="d-flex" role="search">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-success" type="submit">Search</button>
-        </form>
-    </div>
-</nav>
+<div class="">
 
-<div class="container">
+    <nav class="navbar bg-body-tertiary nvar-panel">
+        <div class="container">
+            <a class="navbar-brand" href="#">
+                <img src="{{asset('images/logo-nikken.png')}}" alt="NIKKEN">
+            </a>        
+            {{--<form class="d-flex" role="search">
+            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-success" type="submit">Search</button>
+            </form>--}}
+        </div>
+    </nav>
+
+    <div class="w-100 bg-secondary">
+        <div class="container bg-light">
+            <div class="row py-5">
+
+                <form>
+                    <div class="mb-3">
+                        <label for="exampleInputEmail1" class="form-label">Cupón:</label>
+                        <input type="text" class="form-control w-50" id="cupon">                    
+                    </div>                
+                    <button type="submit" class="btn btn-primary">Búscar</button>
+                </form>
+                
+            </div>
+
+        </div>
+        <div class="container bg-light">
+            <div class="row py-2">
+                <table class="table">
+                    <thead>
+                        <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">First</th>
+                        <th scope="col">Last</th>
+                        <th scope="col">Handle</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                        <th scope="row">1</th>
+                        <td>Mark</td>
+                        <td>Otto</td>
+                        <td>@mdo</td>
+                        </tr>
+                        <tr>
+                        <th scope="row">2</th>
+                        <td>Jacob</td>
+                        <td>Thornton</td>
+                        <td>@fat</td>
+                        </tr>
+                        <tr>
+                        <th scope="row">3</th>
+                        <td colspan="2">Larry the Bird</td>
+                        <td>@twitter</td>
+                        </tr>
+                    </tbody>
+                </table>
+                
+                
+            </div>
+
+        </div>
+    </div>
 
 </div>
+
 
 
 </x-guest-Layout>
