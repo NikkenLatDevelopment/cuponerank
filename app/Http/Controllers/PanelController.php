23be28fd06cf513d5d6a65fa3cd2acb087fe2a9d
gzamudioc <gzamudioc@nikkenlatam.com>
2024-05-21 13:59:37 -0600

    add logs panel

--- app/Http/Controllers/PanelController.php
+++ app/Http/Controllers/PanelController.php
@@ -14,23 +14,34 @@ class PanelController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
         //
         //dd(request()->email);
-        return view('panel.index');        
+        $email = $request->email;
+        return view('panel.index',compact("email"));        
     }
 
     public function searchCoupon(Request $request){
-        
-        $coupon = DB::connection('SQL173')->select('SELECT * FROM PLAN_INFLUENCIA_MK.dbo.ubiSorprende_Cupones WHERE codigoCupon = ?', [$request->coupon]);
-        //$coupon = DB::connection('SQL173')->select('SELECT * FROM PLAN_INFLUENCIA_MK.dbo.ubiSorprende_Cupones');
-        //dd($coupon);
-        if (!empty($coupon)) {
-            return response()->json(['success' => 'Cupón encontrado.', 'data' => $coupon], 200);            
-        }else{
-            return response()->json(['error' => 'No se encontró ningún cupón con ese código.'], 404);
+
+        try{
+
+            $coupon = DB::connection('SQL173')->select('SELECT * FROM PLAN_INFLUENCIA_MK.dbo.ubiSorprende_Cupones WHERE codigoCupon = ?', [$request->coupon]);
+            //$coupon = DB::connection('SQL173')->select('SELECT * FROM PLAN_INFLUENCIA_MK.dbo.ubiSorprende_Cupones');
+            //dd($coupon);
+            $agentName = "test";
+            if (!empty($coupon)) {
+                $this->saveLog($request->coupon,$agentName,$request->agentEmail,"","SEARCH COUPON",date('Y-m-d'),date('H:i:s'));
+                return response()->json(['success' => 'Cupón encontrado.', 'data' => $coupon], 200);            
+            }else{
+                return response()->json(['error' => 'No se encontró ningún cupón con ese código.'], 404);
+            }
+
+        } catch(Excepion $e) {
+            return response()->json(['error' => 'Hubo un problema '.$e], 500);
         }
+        
+        
         
     }
 
@@ -47,6 +58,8 @@ class PanelController extends Controller
         $coupon = DB::connection('SQL173')->update('UPDATE PLAN_INFLUENCIA_MK.dbo.ubiSorprende_Cupones set redimido = 0 WHERE codigoCupon = ?', [$request->coupon]);
         if ($coupon > 0) {
             //echo "Actualización satisfactoria. Número de filas afectadas: $affected";
+            $agentName = "test";
+            $this->saveLog($request->coupon,$agentName,$request->agentEmail,$request->userEmail,"ACTIVATE COUPON",date('Y-m-d'),date('H:i:s'));
             return response()->json(['success' => 'Redimido.'], 200);       
         } else {
             return response()->json(['error' => 'Problema al redimir.'], 404);
@@ -59,6 +72,25 @@ class PanelController extends Controller
         $email = request()->e;
         $url = \URL::temporarySignedRoute("panel.index", now()->addMinutes(1), ['email' => "$email"]);
         return $url;
+
+    }
+
+    public function saveLog($coupon, $agentName, $agentEmail, $clientEmail, $action, $date, $hour){
+        
+        if($action == "SEARCH COUPON"){
+            //dd("saveLog",$coupon,$agentEmail);
+            DB::connection('SQL173')->insert('
+                INSERT INTO PLAN_INFLUENCIA_MK.dbo.Log_Panel_UBI (codigo, nombreAgente, emailAgente, emailUser, accion, fecha, hora)
+                VALUES (?, ?, ?, ?, ?, ?, ?)
+            ', [$coupon, $agentName, $agentEmail, "", "SEARCH COUPON", $date, $hour]);
+        }else if($action == "ACTIVATE COUPON"){
+
+            DB::connection('SQL173')->insert('
+                INSERT INTO PLAN_INFLUENCIA_MK.dbo.Log_Panel_UBI (codigo, nombreAgente, emailAgente, emailUser, accion, fecha, hora)
+                VALUES (?, ?, ?, ?, ?, ?, ?)
+            ', [$coupon, $agentName, $agentEmail, "", "ACTIVATE COUPON", $date, $hour]);
+
+        }
 
     }
 }
