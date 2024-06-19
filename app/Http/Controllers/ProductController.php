a5fea520dc3aff2619d569021e4fc17d61cf4040
avillaSori <155116601+avillaSori@users.noreply.github.com>
2024-06-19 14:32:11 -0600

    Update ProductController.php

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -95,8 +95,8 @@ class ProductController extends Controller
             // Establecer la conexión 'SQL173' y realizar la consulta
             $pais = DB::connection('SQL173')->table('LAT_NIKKEN_TV.dbo.ubiSorprende_Cupones')
                         ->where('email', $email)
-                        ->value('redimido')
-                        >get();
+                        ->value('redimido');
+                       // >get();
 
             //dd($pais);
                         // Comprobar si se obtuvo el campo 'pais' y responder
