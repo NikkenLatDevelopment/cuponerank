7ee1a75add7ce0b3c794c632d01a5095d8c2813c
gzamudioc <gzamudioc@nikkenlatam.com>
2024-06-27 18:48:45 -0600

    600 MINUTES FOR PANEL

--- app/Http/Controllers/PanelController.php
+++ app/Http/Controllers/PanelController.php
@@ -78,7 +78,8 @@ class PanelController extends Controller
         try{
             $email = request()->e;
             $decodedCod = base64_decode($email);
-            $url = \URL::temporarySignedRoute("panel.index", now()->addMinutes(1), ['e' => "$email"]);
+            $url = \URL::temporarySignedRoute("panel.index", now()->addMinutes(600), ['e' => "$email"]);
+            //$url = route("panel.index", ['e' => $email]);
             return $url;
         } catch(Excepion $e) {
             return "error";
